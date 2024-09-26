a3ccd5bf1d9ee8fe108a64a372aa23a9ff0a2f4e
Linuss <tade3d@gmail.com>
2024-09-26 12:24:51 +0200

    Added "Not found"-route.
    When trying to fetch from an unknown route, return "Not found".

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import { Hono } from "hono";
 
 const app = new Hono();
 
+app.notFound((c) => c.json({ message: "Not found", ok: false }, 404));
+
 app.get("/", (c) => {
 	return c.json({
 		data: "data",
